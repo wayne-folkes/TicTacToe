e76684302e7a214f7924a1b33593108d2a26060f
Wayne Folkes <wayne.folkes@gmail.com>
2026-01-27 22:12:21 -0500

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Wayne Folkes on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
